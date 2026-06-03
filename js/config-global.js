633d81721149c7180ba010ec0171cddda0333ce2
Emerpachon3120 <73370070+Emerpachon3120@users.noreply.github.com>
2026-06-03 09:35:58 -0500

    Update config-global.js

--- js/config-global.js
+++ js/config-global.js
@@ -1,10 +1,10 @@
 window.APP_CONFIG = {
-  BACKEND_URL: 'https://script.google.com/macros/s/AKfycbxZqm3NStz3QQCXkVA3vq2xBRYtqUusq0uGlnU9fcVRN6NrQAESE8T1kma48gzPhi0H/exec',
+  BACKEND_URL: 'https://script.google.com/macros/s/AKfycby55QDYjLbZZN4UNzpVMtJG2p2rRg2dA3HLtTQTEScKmvdVTgMj_Px3khCAFGoIlLoL/exec',
   FORMS_SPREAD_ID: '16lGe0Qz0C_6mviquMLsNcFA3spVwrSFKxA5xJP0qv4E',
-  FORMS_SHEET_NAME:'Incidencias',
-  CACHE_VERSION:   '3.2',
-  APP_NAME:        'Gestión TI',
-  ENTIDAD:         'Alcaldía Municipal de Nemocón',
-  RESPONSABLE_TI:  'Emerson Judiño Pachón Ayala',
-  VERSION:         '3.1.0',
+  FORMS_SHEET_NAME: 'Incidencias',
+  CACHE_VERSION:    '3.3',
+  APP_NAME:         'Gestión TI',
+  ENTIDAD:          'Alcaldía Municipal de Nemocón',
+  RESPONSABLE_TI:   'Emerson Judiño Pachón Ayala',
+  VERSION:          '3.1.0',
 };
